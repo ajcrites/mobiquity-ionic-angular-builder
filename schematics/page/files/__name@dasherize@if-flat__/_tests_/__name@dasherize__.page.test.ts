31117c3e5514822f6033c966d02c01aad67e9f1f
Andrew Crites <acrites@mobiquityinc.com>
2018-12-14 12:12:41 -0500

    feat(schematics): Align page generator to common implementations.
    * Use a shared module instead of importing common modules
    * Syntactic updates (e.g. end of line commas)

--- schematics/page/files/__name@dasherize@if-flat__/_tests_/__name@dasherize__.page.test.ts
+++ schematics/page/files/__name@dasherize@if-flat__/_tests_/__name@dasherize__.page.test.ts
@@ -9,7 +9,7 @@ describe('<%= classify(name) %>Page', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ <%= classify(name) %>Page ],
+      declarations: [<%= classify(name) %>Page],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
